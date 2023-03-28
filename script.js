807f0e405b2980184ae1ff433e1c9e829f09c3cb
eric-chambonneau <101542272+eric-chambonneau@users.noreply.github.com>
2023-03-28 22:03:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+//alert("salut bob");
+//console.log("salut bob");
+
+/*commenter 
+plusieurs
+lignes*/
+
+//camelCase
+let maSuperVariable = "Coco";
+console.log(maSuperVariable);
+
+//les variables
+var unTexte = "remplacer un texte";
+console.log(unTexte);
+
+
+//let = on peut changer la valeur
+
+let name = "Bob";
+
+name = "Alice";
+
+//const = on ne peut pas changer la valeur
+const age = 30;
+
+age = 31; //erreur
+
+
+let monChiffre = "10";
+
+monChiffre = "16";
+
+let chaine = "je suis l'une des chaine de caractères";
+
+let nouvelleChaine = "chaine précédente" + chaine;
+console.log(nouvelleChaine);
+
+//contanenation avec backtick (alt gr + 7)
+
+let autreConcatenation = `chaine précédente ${chaine}. voila c'etait ma concatenation`;

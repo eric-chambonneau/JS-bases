88b703a54d2efe470a2037a334edbcaaa66d61f1
eric-chambonneau <101542272+eric-chambonneau@users.noreply.github.com>
2023-03-29 08:13:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -38,3 +38,54 @@ console.log(nouvelleChaine);
 //contanenation avec backtick (alt gr + 7)
 
 let autreConcatenation = `chaine précédente ${chaine}. voila c'etait ma concatenation`;
+
+//**types de données */
+let string = "je suis une chaine de caractères";
+let number = 10;
+let boolean = true // or false;
+
+//**tableaux = array on le délimite par des crochets []*/
+let array = ["je", "suis", "un", "tableau", 46, true];
+
+//**objet = object on le délimite par des accolades {}*/
+
+let object = {
+    prenom: "Bob",
+    age: 30,
+    ville: "Paris",
+}
+
+let arbre 
+
+arbre = "chêne";
+
+console.log(arbre);
+
+// les opérateurs
+
+//console.log(10 + 5);
+//console.log(10 - 5);
+//console.log(10 / 5);
+//console.log(10 * 5);
+
+// puissance/
+//console.log(10 ** 5);
+
+// opérateurs d'affectations
+let total = 0;
+
+total ++ // = (total = total + 1)
+
+total +=5 // = (total = total + 5)
+total -=5 // = (total = total - 5)
+total *=5 // = (total = total * 5)
+
+console.log(total);
+
+// structure de contrôle
+let x = 2;
+let y = 5;
+
+if (x > y) {
+    console.log("x est plus grand que y");
+}
